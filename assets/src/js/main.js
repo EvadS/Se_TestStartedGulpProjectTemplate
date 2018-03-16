c60bc622b75521e57d10b8dea4d0d7627344ae4f
Evgeniy.Skiba <Skiba.Evgeniy@hotmail.com>
2018-03-16 14:43:45 +0200

    completed

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,1 +1,20 @@
 ﻿///для определения списка файлов, содержимое которых необходимо будет включить в итоговый js-файл
+
+// Импортируем jQuery
+//=../bower_components/jquery/dist/jquery.js
+
+//=../bower_components/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap 4
+//=../bower_components/bootstrap/js/dist/util.js
+//=../bower_components/bootstrap/js/dist/alert.js
+//=../bower_components/bootstrap/js/dist/button.js
+//=../bower_components/bootstrap/js/dist/carousel.js
+//=../bower_components/bootstrap/js/dist/collapse.js
+//=../bower_components/bootstrap/js/dist/dropdown.js
+//=../bower_components/bootstrap/js/dist/modal.js
+//=../bower_components/bootstrap/js/dist/scrollspy.js
+//=../bower_components/bootstrap/js/dist/tab.js
+//=../bower_components/bootstrap/js/dist/tooltip.js
+//=../bower_components/bootstrap/js/dist/popover.js
+
